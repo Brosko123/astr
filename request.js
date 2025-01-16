e10b96b5f9dfd31d28bf49f96a90a556bc2881c4
Brosko123 <ytur123@tutamail.com>
2025-01-16 16:47:37 +0530

    Update request.js

--- request.js
+++ request.js
@@ -44,18 +44,18 @@ function compressStream(inputStream, format, quality, grayscale, res, originSize
       sharpInstance
         .toFormat(format, { quality })
         .on("data", (chunk) => {
-         // const buffer = Buffer.from(chunk); // Convert the chunk to a buffer
+          const buffer = Buffer.from(chunk); // Convert the chunk to a buffer
          // processedSize += buffer.length;
-          res.write(chunk);
+          res.send(buffer);
         })
         .on("info", (info) => {
           res.setHeader("X-Original-Size", originSize);
          // res.setHeader("X-Processed-Size", processedSize);
          // res.setHeader("X-Bytes-Saved", originSize - processedSize);
         })
-        .on("end", () => {
+        /*.on("end", () => {
           res.end(); // Finalize the response
-        })
+        })*/
         .on("error", (err) => {
           console.error("Error during compression:", err.message);
           res.status(500).send("Error processing image.");
